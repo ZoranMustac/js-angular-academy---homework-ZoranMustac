6d9452dfb8f743b7ddd061ec428de0e254acb00e
zm2666 <83871614+zm2666@users.noreply.github.com>
2022-07-07 13:27:59 +0200

    Styled data from local storage

--- assignment1/JS/index.js
+++ assignment1/JS/index.js
@@ -8,6 +8,20 @@ post.addEventListener('click', (event) => {
 	reviews();
 });
 
+let style = `
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    color: white;
+    text-align: left;
+    padding: 1vw;
+    padding-left: 2vw;
+    max-width: 30vw;
+    border-radius: 10px;
+    font-size: 1vw;
+    background-color: rgb(196, 54, 54);
+`;
+
 function reviews() {
 	const postReview = document.getElementById('postReview');
 	const newReview = document.createElement('p');
@@ -24,6 +38,12 @@ function reviews() {
 	newRating.innerHTML = info.rating;
 
 	localStorage.setItem('review', JSON.stringify(save));
+	if (localStorage.getItem('review') != null) {
+		newReview.innerHTML = info.review;
+		newRating.innerHTML = info.rating;
+	}
+
+	postReview.style = style;
 	postReview.appendChild(newReview);
 	postReview.appendChild(newRating);
 }
